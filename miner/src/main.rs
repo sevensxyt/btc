44f16d6827fab10ed403fb7e72a38333dfefc212
sevensxyt <sevendotxyt@gmail.com>
2025-09-09 16:04:51 +0800

    feat (miner): mine btc block

--- miner/src/main.rs
+++ miner/src/main.rs
@@ -1,3 +1,35 @@
+use std::{env, process::exit};
+
+use btclib::{types::Block, util::Saveable};
+
 fn main() {
-    println!("Hello, world!");
+    let (Some(path), Some(steps)) = (env::args().nth(1), env::args().nth(2)) else {
+        exit(1);
+    };
+
+    let Ok(steps) = steps.parse::<usize>() else {
+        eprint!("<steps> should be a positive integer");
+        exit(1);
+    };
+
+    let original_block = Block::load_from_file(path).expect("Failed to load block");
+    let mut block = original_block.clone();
+
+    while !block.header.mine(steps).expect("Failed to mine block") {
+        println!("minining...");
+    }
+
+    println!("original: {original_block:#?}");
+    println!(
+        "hash: {}",
+        original_block
+            .hash()
+            .expect("Failed to hash original block")
+    );
+
+    println!("final: {block:#?}");
+    println!(
+        "hash: {}",
+        block.hash().expect("Failed to hash original block")
+    );
 }
