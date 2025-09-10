92f461a9bc98ce4ab630722feb1b161b1866c771
sevensxyt <sevendotxyt@gmail.com>
2025-09-11 01:24:08 +0800

    feat (miner): miner base

--- miner/src/main.rs
+++ miner/src/main.rs
@@ -1,35 +1,77 @@
-use std::{env, process::exit};
+use std::{
+    env::{self},
+    process::exit,
+};
 
-use btclib::{types::Block, util::Saveable};
+use btclib::{crypto::PublicKey, network::Message, types::Block, util::Saveable};
+use clap::Parser;
+use tokio::net::TcpStream;
 
-fn main() {
-    let (Some(path), Some(steps)) = (env::args().nth(1), env::args().nth(2)) else {
-        exit(1);
-    };
+#[derive(Parser)]
+#[command(author, version, about, long_about=None)]
+struct Cli {
+    #[arg(short, long)]
+    address: String,
+    #[arg(short, long)]
+    public_key_file: String,
+}
 
-    let Ok(steps) = steps.parse::<usize>() else {
-        eprint!("<steps> should be a positive integer");
-        exit(1);
-    };
-
-    let original_block = Block::load_from_file(path).expect("Failed to load block");
-    let mut block = original_block.clone();
-
-    while !block.header.mine(steps).expect("Failed to mine block") {
-        println!("minining...");
+struct Miner;
+impl Miner {
+    async fn new(address: String, public_key: PublicKey) -> ! {
+        unimplemented!()
     }
 
-    println!("original: {original_block:#?}");
-    println!(
-        "hash: {}",
-        original_block
-            .hash()
-            .expect("Failed to hash original block")
-    );
+    async fn run(&self) -> ! {
+        unimplemented!()
+    }
 
-    println!("final: {block:#?}");
-    println!(
-        "hash: {}",
-        block.hash().expect("Failed to hash original block")
+    fn spawn_mining_thread(&self) -> ! {
+        unimplemented!()
+    }
+
+    async fn fetch_and_validate_template(&self) -> ! {
+        unimplemented!()
+    }
+
+    async fn fetch_template(&self) -> ! {
+        unimplemented!()
+    }
+
+    async fn validate_template(&self) -> ! {
+        unimplemented!()
+    }
+
+    async fn submit_block(&self, block: Block) -> ! {
+        unimplemented!()
+    }
+}
+
+fn usage() -> ! {
+    eprintln!(
+        "Usage: {} <address> <public_key_file>",
+        env::args().next().unwrap()
     );
+    exit(1)
+}
+
+#[tokio::main]
+async fn main() {
+    let address = env::args().nth(1).unwrap_or_else(|| usage());
+    let public_key_file = env::args().nth(2).unwrap_or_else(|| usage());
+    let Ok(public_key) = PublicKey::load_from_file(&public_key_file) else {
+        eprint!("Error reading public key from file {public_key_file}");
+        exit(1);
+    };
+
+    println!("Connecting to {address} to mine with {public_key:?}");
+    let Ok(mut stream) = TcpStream::connect(&address).await else {
+        eprintln!("Failed to connect to server at {address}");
+        exit(1);
+    };
+
+    println!("requesting work from {address}");
+    let message = Message::FetchTemplate(public_key);
+    // message.send(&mut stream);
+    // todo: asynd send and receive
 }
